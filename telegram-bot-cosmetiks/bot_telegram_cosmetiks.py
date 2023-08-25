eaaced0d59d9ad3dc33bbb4495afff43413f75d7
Valiantsin Ushkevich <vushkevih@gmaol.com>
2023-08-25 14:08:11 +0200

    изменение,что бы бот удалял сообщение пользователя и отвечал

--- telegram-bot-cosmetiks/bot_telegram_cosmetiks.py
+++ telegram-bot-cosmetiks/bot_telegram_cosmetiks.py
@@ -43,7 +43,8 @@ async def commands_start(message : types.Message):
 
 @dp.message_handler(commands=['Режим_работы'])
 async def pizza_open_command(message : types.Message):
-    await bot.send_message(message.from_user.id, 'Вт-Чт с 9:00 до 20:00, Пт-Сб с 10:00 до 23:00')
+    await message.delete()
+    await bot.send_message(message.from_user.id,'Режим работы: Вт-Чт с 9:00 до 20:00, Пт-Сб с 10:00 до 23:00')
 
 @dp.message_handler(commands=['Расположение'])
 async def pizza_place_command(message : types.Message):

8eb24aa95735c294c269f003d4e891b151a2f68b
Valiantsin Ushkevich <vushkevih@gmaol.com>
2023-08-28 14:10:38 +0200

    All of "add client keyboard after command /start"

--- telegram-bot-cosmetiks/keyboards/client_kb.py
+++ telegram-bot-cosmetiks/keyboards/client_kb.py
@@ -1,10 +1,15 @@
-from aiogram.types import ReplyKeyboardMarkup,KeyboardButton,ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
 
 b1 = KeyboardButton('/Режим_работы')
 b2 = KeyboardButton('/Расположение')
 b3 = KeyboardButton('/Меню')
 
-kb_client = ReplyKeyboardMarkup()
+b4 = KeyboardButton('поделиться номером',request_contact=True)                  # спец.кнопка,получает номер
+b5 = KeyboardButton('отправить где я',request_location=True)                    # спец.кнопка,получает локацию
 
-kb_client.add(b1).add(b2).add(b3)
+kb_client = ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True)    # аргумент resize_keyboard=True делает,что кнопки изменяют размер под содержимое(сужаются)
+                                                                                # аргумент one_time_keyboard удаляет клавиатуру,после выбора кнопки(можно вернуть клавиатуру)
+# kb_client = ReplyKeyboardMarkup()                                             # без аргумента кнопки будут просто все одиноковые
+
+kb_client.add(b1).add(b2).add(b3).row(b4, b5)                                   # добавление кнопок в клавиатуру
 
